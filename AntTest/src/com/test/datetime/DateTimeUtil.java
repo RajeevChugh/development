e5235dbf7be158106fc5a76515db52fdc7cb324f
unknown <chug02@lainpuma0229.springer-sbm.com>
2015-07-02 15:55:33 +0530

    this commit will provide build.xml

--- AntTest/src/com/test/datetime/DateTimeUtil.java
+++ AntTest/src/com/test/datetime/DateTimeUtil.java
@@ -6,9 +6,7 @@ public class DateTimeUtil {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Today date is:"+dateNow());
-		System.out.println("printing again:"+dateNow());
-		System.out.println("printing one more time:"+dateNow());
+		System.out.println("now the date is:"+dateNow());
 	}
 
 	
