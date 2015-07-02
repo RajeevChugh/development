914b137123eaa982903f136677709b283e9ed7d3
Rajeev Chugh <rajeev.09chugh@gmail.com>
2015-07-02 14:26:42 +0530

    this is the second commit

--- AntTest/src/com/test/datetime/DateTimeUtil.java
+++ AntTest/src/com/test/datetime/DateTimeUtil.java
@@ -7,7 +7,7 @@ public class DateTimeUtil {
 	public static void main(String[] args) {
 		
 		System.out.println("Today date is:"+dateNow());
-
+		System.out.println("printing again:"+dateNow());
 	}
 
 	
